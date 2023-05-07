76430d266bb29ed9bb2b5f02057eed2bdac66cf7
jfrasinska <j.frasinska@gmail.com>
2023-05-07 21:28:42 +0200

    refactoring 1 gallery js file

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const gallery = galleryItems.map(item => {
+  return `<li>
+              <a class="gallery__item" href="${item.original}">
+                <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+              </a>
+            </li>`;
+});
+
+galleryContainer.insertAdjacentHTML('beforeend', gallery.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
