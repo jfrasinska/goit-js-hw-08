e2688d79d65e370cd21a6d762f48dfc322036949
jfrasinska <j.frasinska@gmail.com>
2023-05-07 22:59:09 +0200

    adding second task js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
 
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+function currentTime(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player.on('timeupdate', throttle(currentTime, 1000));
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime !== null) {
+  const actTime = parseFloat(savedTime);
+  player
+    .setCurrentTime(actTime)
+    .then(() => {
+      player.play();
+    })
+    .catch(error => {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+
+        default:
+          break;
+      }
+    });
+}
